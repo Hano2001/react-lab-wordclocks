231f7cf8ce6f30d839dc0a247b394e0570b47b89
Hano2001 <haraldnorlin93@gmail.com>
2024-07-09 11:40:36 +0200

    Updating time in clock component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
-
+type Time = {
+  seconds: number;
+  minutes: number;
+  hours: number;
+};
 export function Clock(props: { name: string; code: string }) {
-  const [time, setTime] = useState<null | number>();
+  const [time, setTime] = useState<Time>({
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+  });
   useEffect(() => {
-    const currentTime = new Date().getTime();
-    setTime(currentTime);
+    const interval = setInterval(() => {
+      const date = new Date();
+      const seconds = date.getSeconds();
+      const minutes = date.getMinutes();
+      const hours = date.getHours();
+      setTime({ seconds: seconds, minutes: minutes, hours: hours });
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
   return (
     <div>
       {props.name}
-      <br />
+      {time.hours}
+      <span>:</span>
+      {time.minutes}
+      <span>:</span>
+      {time.seconds}
       {props.code}
-      {time};
     </div>
   );
 }
